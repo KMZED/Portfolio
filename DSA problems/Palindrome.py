b556b8ca6ddb89f8adaddbbc92a650dcc7f8a342
Manu <kadiyanmanu@gmail.com>
2026-04-13 15:13:45 +1200

    Daily DSA

---
+++ DSA problems/Palindrome.py
@@ -0,0 +1,17 @@
+s = "A man, a plan, a canal: Panama"
+
+def is_palindrome(s):
+    i,j = 0, len(s) - 1
+    while i < j:
+        while i < j and not s[i].isalnum():
+            i += 1
+        while i < j and not s[j].isalnum():
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
+              
+    return True
+
+print(is_palindrome(s))            
