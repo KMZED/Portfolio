cab51230541162311e866e7fd33a6adc280dd7ad
Manu <kadiyanmanu@gmail.com>
2026-04-16 16:31:49 +1200

    daily dsa

--- DSA problems/Linked_list.py
+++ DSA problems/Linked_list.py
@@ -21,16 +21,35 @@ class singly_linked_list:
         while temp.next:
             temp = temp.next
         temp.next = new_node    
-
+    def insert_after_target(Self,target,data):
+        temp = Self.head
+        while temp:
+            if temp.data == target:
+                new_node = node(data)
+                new_node.next = temp.next
+                temp.next = new_node
+                return
+            temp = temp.next
+        print("Target not found")    
+    def reverse(Self):
+        prev = None
+        curr = Self.head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        Self.head = prev    
     def display(Self):
         temp = Self.head    
         while temp:
             print(temp.data, end = "->")   
             temp = temp.next 
         print("None")    
-
 sll = singly_linked_list()
 sll.insert_at_beginning(1)
 sll.insert_at_end(2)
-sll.insert_at_end(3)
+sll.insert_after_target(1,3)
+sll.insert_after_target(3,4)
+sll.reverse()
 sll.display()
