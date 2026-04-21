a9d308269bea95dbe92a86742a0f0c4c99c621f4
Manu <kadiyanmanu@gmail.com>
2026-04-22 09:50:19 +1200

    Daily dsa

---
+++ DSA problems/Linked_list_headtail.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+class linked_list:
+    def __init__(self,value):
+        new_node = Node(value)   
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.value, end = " => ")
+            temp = temp.next                    
+        print("None")
+    def pop(self):
+        # print(self.length)
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        temp = self.head
+        while temp.next is not None:
+            if temp.next.next == None:
+                prev = temp.next
+                temp.next = None
+                self.tail = temp
+                self.length -= 1
+                return prev         
+            temp = temp.next
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        # temp = self.head
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        else:
+            temp = self.head
+            self.head = temp.next
+            self.length -= 1
+            return temp
+ll = linked_list(10)   
+ll.print()         
+ll.append(12)
+ll.append(13)
+ll.print()
+ll.pop()
+ll.print()
+ll.prepend(19)
+ll.print()
+ll.pop_first()
+ll.print()
